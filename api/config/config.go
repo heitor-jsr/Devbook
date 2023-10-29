7acc68a379b538631d92e06fcb0bfbe4fcaa632a
heitor-jsr <heitorscalon@gmail.com>
2023-10-29 11:55:41 -0300

    chore: implement .env variable and ensure its loading correctly

---
+++ api/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+// para carregar essas variaveis de ambiente para dentro do go, nós precisamos de um pacote que se chama dotenv. ele é usado apenas para ler esses arquivos .env.
+
+var (
+	ConnStr = ""
+	Port = 0
+)
+
+// vai carregar as variaveis de ambiente dentro da nossa aplicação. não recebe e nem retorna parametros. ela só vai alterar as variaveis de ambiente criadas antes dela, que vão estar disponíveis para a api toda. basicamente, a função abaixo vai só jogar valores dentro das variaveis de ambiente.
+func Load() {
+	var err error
+
+	// abaixo usamos uma função do pacote dotenv que vai ler as informações do arquivo .env e disponibilizar elas no nosso ambiente de desenvolvimento. se ocorrer algum erro, a execução do programa deve ser encerrada, e a API nem deve subir. por isso do log.fatal. quem chama isso é o package main. se tudo der certo, as informações do .env já estão disponíveis para serem usadas e podem ser inseridas nas variaveis inicializadas acima.
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	Port, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		Port = 9000
+	}
+
+	ConnStr = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+}
--- api/main.go
+++ api/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"api/config"
 	"api/src/router"
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 )
 
 // é esse o arquivo que vai ser o responsavel por executar o projeto. é ele que chama os outros pacotes, como o config, router, etc.
 
 func main() {
-	fmt.Println("API is running on port 5000")
+	// para carregar os arquivos .env, basta executarmos a função load. para ter certeza que tudo deu certo, precisamos apenas dar um log na porta. por fim, para dar o listen and serve na porta do .env, basta executar o seguinte comando:
+	config.Load()
+
+	fmt.Printf("API is running on port %d", config.Port)
 
 	r := router.Generator()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
