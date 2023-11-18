5c0ec07f9d6d1afbbf1ef61a9063616dbb38b659
heitor-jsr <heitorscalon@gmail.com>
2023-11-18 18:45:17 -0300

    refactor: added envPath to the config file

--- api/config/config.go
+++ api/config/config.go
@@ -20,9 +20,10 @@ var (
 // vai carregar as variaveis de ambiente dentro da nossa aplicação. não recebe e nem retorna parametros. ela só vai alterar as variaveis de ambiente criadas antes dela, que vão estar disponíveis para a api toda. basicamente, a função abaixo vai só jogar valores dentro das variaveis de ambiente.
 func Load() {
 	var err error
+	envPath := "/home/dornzak/devbook/api/.env"
 
 	// abaixo usamos uma função do pacote dotenv que vai ler as informações do arquivo .env e disponibilizar elas no nosso ambiente de desenvolvimento. se ocorrer algum erro, a execução do programa deve ser encerrada, e a API nem deve subir. por isso do log.fatal. quem chama isso é o package main. se tudo der certo, as informações do .env já estão disponíveis para serem usadas e podem ser inseridas nas variaveis inicializadas acima.
-	if err = godotenv.Load(); err != nil {
+	if err = godotenv.Load(envPath); err != nil {
 		log.Fatal(err)
 	}
 
